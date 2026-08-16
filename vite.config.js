5a9bd6488b73ee84310f2753189aa6a8b71e7005
Amine Guellil <guellilamine06@gmail.com>
2026-08-16 10:05:19 +0100

    refactor: enhance Vite configuration with React and Tailwind plugins

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // فصل مكتبات React الأساسية
+            if (
+              id.includes("react/") ||
+              id.includes("react-dom/") ||
+              id.includes("scheduler")
+            ) {
+              return "react-vendor";
+            }
+            // فصل مكتبة الأيقونات
+            if (id.includes("lucide-react")) {
+              return "lucide";
+            }
+          }
+        },
+      },
+    },
+  },
+});
